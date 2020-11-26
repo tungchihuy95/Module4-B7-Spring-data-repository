3453bb4bb8ebf33129d94c9289df86effe09979a
tungchihuy95 <tungjpvn95@gmail.com>
2020-11-26 22:27:46 +0700

    customerprovince

---
+++ src/main/java/repository/ProvinceRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Province;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProvinceRepository extends PagingAndSortingRepository<Province, Long> {
+
+}
---
+++ src/main/java/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,48 @@
+package service.impl;
+
+import model.Customer;
+import model.Province;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import repository.CustomerRepository;
+import repository.ProvinceRepository;
+import service.CustomerService;
+
+
+
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.delete(id);
+    }
+
+    @Override
+    public Iterable<Customer> findAllByProvince(Province province) {
+        return customerRepository.findAllByProvince(province);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstname, pageable);
+    }
+}
